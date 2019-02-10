e9f3802c82b5355cbace009007a6d4568916be4e
Saikat Bhattacharyya <saikat01244569@gmail.com>
2019-02-10 19:08:48 +0530

    genesis commit

---
+++ src/app/chat-info/chat-info.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { ChatService } from '../chat.service';
+
+@Component({
+  selector: 'app-chat-info',
+  templateUrl: './chat-info.component.html',
+  styleUrls: ['./chat-info.component.css']
+})
+export class ChatInfoComponent implements OnInit {
+
+  userChats$;
+
+  constructor(public auth: AuthService, public cs: ChatService) {
+  }
+
+  ngOnInit() {
+    this.userChats$ = this.cs.getUserChats();
+  }
+}
---
+++ src/app/chat.service.ts
@@ -0,0 +1,125 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { firestore } from 'firebase/app';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable, combineLatest, of } from 'rxjs';
+import { AuthService } from './shared/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(
+    private afs: AngularFirestore,
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  get(chatId) {
+    return this.afs
+      .collection<any>('chats')
+      .doc(chatId)
+      .snapshotChanges()
+      .pipe(
+        map(doc => {
+          return { id: doc.payload.id, ...doc.payload.data() };
+        })
+      );
+  }
+
+  getUserChats() {
+    return this.auth.userData.pipe(
+      switchMap(user => {
+        return this.afs
+          .collection('chats', ref => ref.where('uid', '==', this.auth.userData.uid))
+          .snapshotChanges()
+          .pipe(
+            map(actions => {
+              return actions.map(a => {
+                const data: Object = a.payload.doc.data();
+                const id = a.payload.doc.id;
+                return { id, ...data };
+              });
+            })
+          );
+      })
+    );
+  }
+
+  async create(user_id) {
+    const uid = user_id;
+
+    const data = {
+      uid,
+      createdAt: Date.now(),
+      count: 0,
+      messages: []
+    };
+
+    const docRef = await this.afs.collection('chats').add(data);
+
+    return this.router.navigate(['chats', docRef.id]);
+  }
+
+  async sendMessage(user_id, chatId, content) {
+    const uid = user_id;
+
+    const data = {
+      uid,
+      content,
+      createdAt: Date.now()
+    };
+
+    if (uid) {
+      const ref = this.afs.collection('chats').doc(chatId);
+      return ref.update({
+        messages: firestore.FieldValue.arrayUnion(data)
+      });
+    }
+  }
+
+  async deleteMessage(chat, msg) {
+    const { uid } = await this.auth.userData.uid;
+
+    const ref = this.afs.collection('chats').doc(chat.id);
+    console.log(msg);
+    if (chat.uid === uid || msg.uid === uid) {
+      // Allowed to delete
+      delete msg.user;
+      return ref.update({
+        messages: firestore.FieldValue.arrayRemove(msg)
+      });
+    }
+  }
+
+  joinUsers(chat$: Observable<any>) {
+    let chat;
+    const joinKeys = {};
+
+    return chat$.pipe(
+      switchMap(c => {
+        // Unique User IDs
+        chat = c;
+        const uids = Array.from(new Set(c.messages.map(v => v.uid)));
+
+        // Firestore User Doc Reads
+        const userDocs = uids.map(u =>
+          this.afs.doc(`users/${u}`).valueChanges()
+        );
+
+        return userDocs.length ? combineLatest(userDocs) : of([]);
+      }),
+      map(arr => {
+        arr.forEach(v => (joinKeys[(<any>v).uid] = v));
+        chat.messages = chat.messages.map(v => {
+          return { ...v, user: joinKeys[v.uid] };
+        });
+
+        return chat;
+      })
+    );
+  }
+
+}
